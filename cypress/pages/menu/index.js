c903acb2edc1e936b40d68ae2470f57403603c4f
francisgobbi <francis.gobbi@db.tec.br>
2024-08-12 05:10:35 -0300

    trabalho final web e, cypress

---
+++ cypress/pages/menu/index.js
@@ -0,0 +1,12 @@
+class Menu{
+
+    irParaProdutos() {
+        cy.contains(`Products`).click()
+    }
+    irParaLoginCadastro() {
+        cy.contains('Signup ').click() 
+    }
+
+}
+
+export default new Menu()
